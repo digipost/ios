b891c6411d8675666311caad67e4e115619943ec
Eivind R. Bøhler <eivind.bohler@shortcut.no>
2013-12-09 13:06:36 +0100

    Added Folders View Controller.

---
+++ Digipost/SHCFoldersViewController.h
@@ -0,0 +1,13 @@
+//
+//  SHCFoldersViewController.h
+//  Digipost
+//
+//  Created by Eivind Bohler on 09.12.13.
+//  Copyright (c) 2013 Shortcut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHCFoldersViewController : UITableViewController
+
+@end
