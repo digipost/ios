f431f32f496e90defbc808b2595f19c711093de5
Eivind R. Bøhler <eivind.bohler@shortcut.no>
2013-12-10 14:14:11 +0100

    Project cleanups, added CocoaLumberjack logging, colors in Xcode debugger console.

--- Digipost/SHCAppDelegate.m
+++ Digipost/SHCAppDelegate.m
@@ -8,22 +8,28 @@
 
 #import "SHCAppDelegate.h"
 #import <HockeySDK/HockeySDK.h>
+#import <DDASLLogger.h>
+#import <DDTTYLogger.h>
+#import <DDFileLogger.h>
 
 @interface SHCAppDelegate () <BITHockeyManagerDelegate>
+
+@property (strong, nonatomic) DDFileLogger *fileLogger;
 
 @end
 
 @implementation SHCAppDelegate
 
+#pragma mark - UIApplicationDelegate
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    [self setupHockeySDK];
+
+    [self setupCocoaLumberjack];
+
     [[UINavigationBar appearance] setBarStyle:UIBarStyleBlackOpaque];
     [[UINavigationBar appearance] setBarTintColor:[UIColor colorWithRed:229.0/255.0 green:42.0/255.0 blue:19.0/255.0 alpha:1.0]];
-
-    [[BITHockeyManager sharedHockeyManager] configureWithBetaIdentifier:__HOCKEY_BETA_IDENTIFIER__
-                                                         liveIdentifier:__HOCKEY_LIVE_IDENTIFIER__
-                                                               delegate:self];
-    [[BITHockeyManager sharedHockeyManager] startManager];
 
     if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
         UISplitViewController *splitViewController = (UISplitViewController *)self.window.rootViewController;
@@ -58,6 +64,38 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+#pragma mark - Private methods
+
+- (void)setupHockeySDK
+{
+    [[BITHockeyManager sharedHockeyManager] configureWithBetaIdentifier:__HOCKEY_BETA_IDENTIFIER__
+                                                         liveIdentifier:__HOCKEY_LIVE_IDENTIFIER__
+                                                               delegate:self];
+    [[BITHockeyManager sharedHockeyManager] startManager];
+}
+
+- (void)setupCocoaLumberjack
+{
+    // Enable Apple System Logger (log messages appear in the Console.app)
+    [DDLog addLogger:[DDASLLogger sharedInstance]];
+
+    // Enable Xcode debugger console (TTY) logger
+    [DDLog addLogger:[DDTTYLogger sharedInstance]];
+
+    // If you want nice colors in Xcode's debugger console,
+    // go to https://github.com/robbiehanson/XcodeColors and follow instructions
+    // on how to install the neccesary Xcode plugin.
+    [[DDTTYLogger sharedInstance] setColorsEnabled:YES];
+
+    // Enable logging to file
+    self.fileLogger = [[DDFileLogger alloc] init];
+    self.fileLogger.rollingFrequency = 60 * 60 * 24; // 24 hour rolling
+    self.fileLogger.logFileManager.maximumNumberOfLogFiles = 7;
+    [self.fileLogger rollLogFileWithCompletionBlock:^{
+        [DDLog addLogger:self.fileLogger];
+    }];
 }
 
 @end
