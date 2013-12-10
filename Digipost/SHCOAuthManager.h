ca8afe40f253079cc798b38972a31364fe5e0d03
Eivind R. Bøhler <eivind.bohler@shortcut.no>
2013-12-10 12:45:14 +0100

    Added OAuth manager.

---
+++ Digipost/SHCOAuthManager.h
@@ -0,0 +1,13 @@
+//
+//  SHCOAuthManager.h
+//  Digipost
+//
+//  Created by Eivind Bohler on 10.12.13.
+//  Copyright (c) 2013 Shortcut. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SHCOAuthManager : NSObject
+
+@end
---
+++ Digipost/SHCOAuthManager.m
@@ -0,0 +1,13 @@
+//
+//  SHCOAuthManager.m
+//  Digipost
+//
+//  Created by Eivind Bohler on 10.12.13.
+//  Copyright (c) 2013 Shortcut. All rights reserved.
+//
+
+#import "SHCOAuthManager.h"
+
+@implementation SHCOAuthManager
+
+@end
