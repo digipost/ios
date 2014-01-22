e00aab45839337239a8cab839d0920402950eb76
Eivind R. Bøhler <eivind.bohler@shortcut.no>
2014-01-22 10:51:32 +0100

    Added a superclass for SHCAttachment and SHCReceipt to inherit from.

---
+++ Digipost/SHCBaseEncryptedModel.h
@@ -0,0 +1,13 @@
+//
+//  SHCBaseEncryptedModel.h
+//  Digipost
+//
+//  Created by Eivind Bohler on 20.01.14.
+//  Copyright (c) 2014 Shortcut. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+@interface SHCBaseEncryptedModel : NSManagedObject
+
+@end
---
+++ Digipost/SHCBaseEncryptedModel.m
@@ -0,0 +1,13 @@
+//
+//  SHCBaseEncryptedModel.m
+//  Digipost
+//
+//  Created by Eivind Bohler on 20.01.14.
+//  Copyright (c) 2014 Shortcut. All rights reserved.
+//
+
+#import "SHCBaseEncryptedModel.h"
+
+@implementation SHCBaseEncryptedModel
+
+@end
