8609ad160f8e0961216fccdc5fe0a2a5503aad71
Håkon Bogen <hakon.bogen@shortcut.no>
2014-02-06 13:19:43 +0100

    setting correct color on selection icon for receipts

--- Digipost/SHCReceiptTableViewCell.m
+++ Digipost/SHCReceiptTableViewCell.m
@@ -27,7 +27,7 @@ NSString *const kReceiptTableViewCellIdentifier = @"ReceiptCellIdentifier";
     [super setSelected:selected animated:animated];
 
     self.selectionStyle = self.isEditing && selected ? UITableViewCellSelectionStyleNone : UITableViewCellSelectionStyleDefault;
-    self.tintColor = self.isEditing ? [UIColor colorWithRed:13.0/255.0 green:122.0/255.0 blue:1.0 alpha:1.0] : [UIColor whiteColor];
+    self.tintColor = self.isEditing ? [UIColor colorWithRed:64.0/255.0 green:66.0/255.0 blue:69.0/255.0 alpha:1.0] : [UIColor whiteColor];
 }
 
 @end
