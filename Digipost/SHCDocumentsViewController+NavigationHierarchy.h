5b92e6e3eaa66f7f480d7152ceb0d5903b26ee41
Håkon Bogen <hakon.bogen@shortcut.no>
2014-06-17 09:29:02 +0200

    refactoring navgiation at start

---
+++ Digipost/SHCDocumentsViewController+NavigationHierarchy.h
@@ -0,0 +1,15 @@
+//
+//  SHCDocumentsViewController+NavigationHierarchy.h
+//  Digipost
+//
+//  Created by Håkon Bogen on 17.06.14.
+//  Copyright (c) 2014 Posten. All rights reserved.
+//
+
+#import "SHCDocumentsViewController.h"
+
+@interface SHCDocumentsViewController (NavigationHierarchy)
+
+- (void)addAccountsAnFoldersVCToDoucmentHierarchy;
+
+@end
---
+++ Digipost/SHCDocumentsViewController+NavigationHierarchy.m
@@ -0,0 +1,37 @@
+//
+//  SHCDocumentsViewController+NavigationHierarchy.m
+//  Digipost
+//
+//  Created by Håkon Bogen on 17.06.14.
+//  Copyright (c) 2014 Posten. All rights reserved.
+//
+
+#import "SHCDocumentsViewController+NavigationHierarchy.h"
+#import "POSAccountViewController.h"
+#import "SHCFoldersViewController.h"
+
+@implementation SHCDocumentsViewController (NavigationHierarchy)
+- (void)addAccountsAnFoldersVCToDoucmentHierarchy
+{
+    if ([self.navigationController.viewControllers[1] isMemberOfClass:[POSAccountViewController class]] == NO) {
+        POSAccountViewController *accountViewController = [self.storyboard instantiateViewControllerWithIdentifier:kAccountViewControllerIdentifier];
+        SHCFoldersViewController *folderViewController = [self.storyboard instantiateViewControllerWithIdentifier:kFoldersViewControllerIdentifier];
+
+        NSMutableArray *newViewControllerArray = [NSMutableArray array];
+        // add account vc as second view controller in navigation controller
+        UIViewController *loginViewController = self.navigationController.viewControllers[0];
+
+        SHCDocumentsViewController *currentViewController = self.navigationController.viewControllers.lastObject;
+
+        [newViewControllerArray addObject:loginViewController];
+        [newViewControllerArray addObject:accountViewController];
+        [newViewControllerArray addObject:folderViewController];
+        [newViewControllerArray addObject:currentViewController];
+
+        folderViewController.selectedMailBoxDigipostAdress = currentViewController.mailboxDigipostAddress;
+
+        [self.navigationController setViewControllers:newViewControllerArray
+                                             animated:YES];
+    }
+}
+@end
