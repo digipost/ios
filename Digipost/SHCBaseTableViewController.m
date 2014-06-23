45696e3b505d7cb75fdb8883c452807a573af94e
Håkon Bogen <hakon.bogen@shortcut.no>
2014-06-23 17:13:49 +0200

    fix interactive pop gesture crash

--- Digipost/SHCBaseTableViewController.m
+++ Digipost/SHCBaseTableViewController.m
@@ -27,7 +27,7 @@
 #import "UIViewController+BackButton.h"
 #import "SHCLetterViewController.h"
 
-@interface SHCBaseTableViewController () <NSFetchedResultsControllerDelegate,UIGestureRecognizerDelegate>
+@interface SHCBaseTableViewController () <NSFetchedResultsControllerDelegate, UIGestureRecognizerDelegate>
 
 @end
 
@@ -46,7 +46,7 @@
     // This line makes the tableview hide its separator lines for empty cells
     self.tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
     self.navigationController.interactivePopGestureRecognizer.delegate = self;
-
+    [self.navigationController.interactivePopGestureRecognizer setCancelsTouchesInView:NO];
 
     self.refreshControl = [[UIRefreshControl alloc] init];
     [self.refreshControl addTarget:self
@@ -211,7 +211,7 @@
         [self performSelector:@selector(updateFolders)];
     }
 
-   [self.tableView reloadData];
+    [self.tableView reloadData];
     [self updateNavbar];
 }
 
@@ -246,9 +246,8 @@
                                                                           attributes:attributes];
 }
 
-- (void)configureCell:(UITableViewCell*)cell atIndexPath:(NSIndexPath*)indexPath
+- (void)configureCell:(UITableViewCell *)cell atIndexPath:(NSIndexPath *)indexPath
 {
-    
 }
 // done
 - (void)updateRefreshControlTextRefreshing:(BOOL)refreshing
@@ -275,29 +274,29 @@
                                                                           attributes:attributes];
 }
 //    - (void)controllerWillChangeContent:(NSFetchedResultsController *)controller {
-//    
+//
 //    // The fetch controller is about to start sending change notifications, so prepare the table view for updates.
 //    [self.tableView beginUpdates];
 //}
 //
 //- (void)controller:(NSFetchedResultsController *)controller didChangeObject:(id)anObject atIndexPath:(NSIndexPath *)indexPath forChangeType:(NSFetchedResultsChangeType)type newIndexPath:(NSIndexPath *)newIndexPath {
-//    
+//
 //    UITableView *tableView = self.tableView;
-//    
+//
 //    switch(type) {
-//            
+//
 //        case NSFetchedResultsChangeInsert:
 //            [tableView insertRowsAtIndexPaths:@[newIndexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
 //            break;
-//            
+//
 //        case NSFetchedResultsChangeDelete:
 //            [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
 //            break;
-//            
+//
 //        case NSFetchedResultsChangeUpdate:
 //            [self configureCell:[tableView cellForRowAtIndexPath:indexPath] atIndexPath:indexPath];
 //            break;
-//            
+//
 //        case NSFetchedResultsChangeMove:
 //            [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
 //            [tableView insertRowsAtIndexPaths:@[newIndexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
@@ -308,11 +307,11 @@
 //- (void)controller:(NSFetchedResultsController *)controller didChangeSection:(id <NSFetchedResultsSectionInfo>)sectionInfo atIndex:(NSUInteger)sectionIndex forChangeType:(NSFetchedResultsChangeType)type
 //{
 //    switch(type) {
-//            
+//
 //        case NSFetchedResultsChangeInsert:
 //            [self.tableView insertSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationAutomatic];
 //            break;
-//            
+//
 //        case NSFetchedResultsChangeDelete:
 //            [self.tableView deleteSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationAutomatic];
 //            break;
