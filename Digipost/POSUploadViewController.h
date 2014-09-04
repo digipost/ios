1aeb6f5f284eb98c1c2660bb58418f6bdca7d06d
Håkon Bogen <hakon.bogen@shortcut.no>
2014-09-04 15:02:42 +0200

    add new controller for uploading

---
+++ Digipost/POSUploadViewController.h
@@ -0,0 +1,13 @@
+//
+//  POSUploadViewController.h
+//  Digipost
+//
+//  Created by Håkon Bogen on 04.09.14.
+//  Copyright (c) 2014 Posten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface POSUploadViewController : UIViewController
+
+@end
