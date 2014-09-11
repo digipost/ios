f6bd0d43447c5da36a236fe4ce44de3bd087e0a9
Håkon Bogen <hakon.bogen@shortcut.no>
2014-09-11 15:24:31 +0200

    add view for uploading files in document (first Swift file in project!)

---
+++ Digipost/POSUploadTableViewHeaderView.swift
@@ -0,0 +1,22 @@
+//
+//  POSUploadTableViewHeaderView.swift
+//  Digipost
+//
+//  Created by Håkon Bogen on 11.09.14.
+//  Copyright (c) 2014 Posten. All rights reserved.
+//
+
+import UIKit
+
+class POSUploadTableViewHeaderView: UITableViewHeaderFooterView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect)
+    {
+        // Drawing code
+    }
+    */
+
+}
