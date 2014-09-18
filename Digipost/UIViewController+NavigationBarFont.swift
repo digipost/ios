94fc105eab6931ceee4dba3b231079183d72fce7
Håkon Bogen <hakon.bogen@shortcut.no>
2014-09-18 16:47:19 +0200

    remove unused file

--- Digipost/UIViewController+NavigationBarFont.swift
+++
@@ -1,9 +0,0 @@
-//
-//  UIViewController+NavigationBarFont.swift
-//  Digipost
-//
-//  Created by Håkon Bogen on 18.09.14.
-//  Copyright (c) 2014 Posten. All rights reserved.
-//
-
-import Foundation
