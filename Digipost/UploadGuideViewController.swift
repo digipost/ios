6c8ebd4e6310b20dfbb5e3d671712f1491f26308
Håkon Bogen <hakon.bogen@shortcut.no>
2014-09-24 12:57:05 +0200

    update layout for upload guide

--- Digipost/UploadGuideViewController.swift
+++ Digipost/UploadGuideViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class UploadGuideViewController: UIViewController {
+    @IBOutlet weak var uploadImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         NSNotificationCenter.defaultCenter().addObserverForName("kFolderViewControllerNavigatedInList", object: nil, queue: nil) { note in
            self.dismissViewControllerAnimated(false, completion: nil)
         }
+        let identifier = NSLocale.autoupdatingCurrentLocale().localeIdentifier
+        let segments = identifier.componentsSeparatedByString("_")
+        
+        if let localeName = segments[0] as String? {
+            if (localeName.lowercaseString == "nb"){
+                uploadImage.image = UIImage(named: "Lastopp_norsk_vertikal")
+            }else {
+                let image = UIImage(named: "Lastopp_engelsk_vertikal")
+                uploadImage.image = UIImage(named: "Lastopp_engelsk_vertikal")
+            }
+            view.updateConstraints()
+            
+        }
+        
+//        NSString *identifier = [[NSLocale autoupdatingCurrentLocale] localeIdentifier];
+//        NSArray *segments = [identifier componentsSeparatedByString:@"_"];
+//        
+//        if ( [segments count] > 1 ) {
+//            identifier = segments[0];
+//        } else {
+//            identifier = identifier;
+//        }
+//        
+//        NSString *fullname = [NSString stringWithFormat:@"%@-%@", name, identifier];
+//        UIImage *image = [UIImage imageNamed:fullname];
+//        
+//        if ( image ) {
+//            return image;
+//        }
     }
     deinit {
         NSNotificationCenter.defaultCenter().removeObserver(self, name: "kFolderViewControllerNavigatedInList", object: nil)
