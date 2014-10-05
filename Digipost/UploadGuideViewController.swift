3716c56770bdd4d609c42a99dd6b752d95d313a6
Håkon Bogen <hakon.bogen@shortcut.no>
2014-10-05 14:04:51 +0200

    fix localization issue when rotating on iphone

--- Digipost/UploadGuideViewController.swift
+++ Digipost/UploadGuideViewController.swift
@@ -14,6 +14,7 @@ class UploadGuideViewController: UIViewController {
     @IBOutlet weak var horizontalUploadImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.title = NSLocalizedString("upload guide navgationtiem title", comment: "title for navigation item on upload")
         self.uploadImage.accessibilityLabel = NSLocalizedString("upload guide image accessability hint", comment: "when user taps on image, this text should be read")
         self.uploadImage.isAccessibilityElement = true
         NSNotificationCenter.defaultCenter().addObserverForName("kFolderViewControllerNavigatedInList", object: nil, queue: nil) { note in
@@ -32,13 +33,11 @@ class UploadGuideViewController: UIViewController {
         NSNotificationCenter.defaultCenter().removeObserver(self, name: "kFolderViewControllerNavigatedInList", object: nil)
     }
     func setImageForOrientation(forOrientation: UIInterfaceOrientation){
-        
         if let horizontalImage = horizontalUploadImage {
             if (UIInterfaceOrientationIsLandscape(forOrientation)){
                 horizontalImage.hidden = false
             } else {
                 horizontalImage.hidden = true
-                
             }
         }
         if let verticalImage = uploadImage {
@@ -55,6 +54,7 @@ class UploadGuideViewController: UIViewController {
             uploadImage.image = UIImage.localizedImage(UIInterfaceOrientation.Portrait)
         }else {
             uploadImage.image = UIImage.localizedImage(toInterfaceOrientation)
+            horizontalUploadImage.image = UIImage.localizedImage(toInterfaceOrientation)
             setImageForOrientation(toInterfaceOrientation)
             
         }
