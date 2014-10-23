199f2576f5b9de76a9fb27348d5e90a09f2db226
Håkon Bogen <hakon.bogen@shortcut.no>
2014-10-23 13:30:17 +0200

    add new images for letter actions

---
+++ Digipost/UIToolbar+Actions.swift
@@ -0,0 +1,14 @@
+//
+//  UIToolbar+Actions.swift
+//  Digipost
+//
+//  Created by Håkon Bogen on 23/10/14.
+//  Copyright (c) 2014 Posten. All rights reserved.
+//
+
+import UIKit
+
+extension UIToolbar {
+    
+ 
+}
