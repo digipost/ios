8de05d5a739323db83902cd38b4802cde5faf372
Håkon Bogen <hakon.bogen@shortcut.no>
2014-10-28 11:53:03 +0100

    only use one bridging header & add convenience method for invoice

---
+++ Digipost/POSInvoice+Methods.swift
@@ -0,0 +1,17 @@
+//
+//  POSInvoice+Methods.swift
+//  Digipost
+//
+//  Created by Håkon Bogen on 28/10/14.
+//  Copyright (c) 2014 Posten. All rights reserved.
+//
+
+import UIKit
+
+extension POSInvoice {
+    
+    func titleForInvoiceButtonLabel() -> String {
+        
+    }
+ 
+}
