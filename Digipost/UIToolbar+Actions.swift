69d37812826d05b1aa5abd4c42d186c181bf98c1
Håkon Bogen <hakon.bogen@shortcut.no>
2014-11-04 12:26:24 +0100

    add localized accessibility labels for toolbar buttons

--- Digipost/UIToolbar+Actions.swift
+++ Digipost/UIToolbar+Actions.swift
@@ -17,6 +17,7 @@ extension UIToolbar {
         let image = UIImage(named: "Info.pdf")
         
         let infoBarButtonItem = UIBarButtonItem(image: UIImage(named: "Info"), style: UIBarButtonItemStyle.Done, target: letterViewController, action: Selector("didTapInformationBarButtonItem:"))
+        infoBarButtonItem.accessibilityLabel = NSLocalizedString("actions toolbar info accessibility label", comment: "read when user taps the info button on voice over")
         let flexibleSpaceBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.FlexibleSpace, target: nil, action: nil)
         
         let items = NSMutableArray()
@@ -47,9 +48,11 @@ extension UIToolbar {
         invoiceButton.setTitleColor(UIColor.digipostSpaceGrey(), forState: UIControlState.Normal)
         invoiceButton.titleLabel?.font = UIFont.systemFontOfSize(15)
         invoiceButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 3, right: 15)
+        invoiceButton.accessibilityLabel = NSLocalizedString("actions toolbar invoice accessibility label", comment: "read when user taps invoice button on voice over")
         let invoiceBarButtonItem = UIBarButtonItem(customView: invoiceButton)
         
         let moreOptionsBarButtonItem = UIBarButtonItem(image: UIImage(named: "More"), style: UIBarButtonItemStyle.Done, target: letterViewController, action: Selector("didTapMoreOptionsBarButtonItem:"))
+        
         
         items.addObject(invoiceBarButtonItem)
         items.addObject(flexibleSpaceBarButtonItem)
@@ -64,11 +67,19 @@ extension UIToolbar {
         
         let moveDocumentBarButtonItem = UIBarButtonItem(image: UIImage(named: "Move"), style: UIBarButtonItemStyle.Done, target: letterViewController, action: Selector("didTapMoveDocumentBarButtonItem:"))
         
+        moveDocumentBarButtonItem.accessibilityLabel = NSLocalizedString("actions toolbar move accessibility label", comment: "read when user taps move button on voice over")
+        
         let deleteDocumentBarButtonItem = UIBarButtonItem(image: UIImage(named: "Delete"), style: UIBarButtonItemStyle.Done, target: letterViewController, action: Selector("didTapDeleteDocumentBarButtonItem:"))
+        
+        deleteDocumentBarButtonItem.accessibilityLabel = NSLocalizedString("actions toolbar delete accessibility label", comment: "read when user taps delete button on voice over")
         
         let renameDocumentBarButtonItem = UIBarButtonItem(image: UIImage(named: "New name"), style: UIBarButtonItemStyle.Done, target: letterViewController, action: Selector("didTapRenameDocumentBarButtonItem:"))
         
+        renameDocumentBarButtonItem.accessibilityLabel = NSLocalizedString("actions toolbar rename accessibility label", comment: "read when user taps rename button on voice over")
+        
         let openDocumentBarButtonItem = UIBarButtonItem(image: UIImage(named: "Open_in"), style: UIBarButtonItemStyle.Done, target: letterViewController, action: Selector("didTapOpenDocumentInExternalAppBarButtonItem:"))
+        
+        openDocumentBarButtonItem.accessibilityLabel = NSLocalizedString("actions toolbar openIn accessibility label", comment: "read when user taps openIn button on voice over")
         
         items.addObject(moveDocumentBarButtonItem)
         items.addObject(flexibleSpaceBarButtonItem)
