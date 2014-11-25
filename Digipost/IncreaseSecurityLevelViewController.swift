ac64bc9fdb519fcc533f169327b5aec077c3a334
Håkon Bogen <hakon.bogen@shortcut.no>
2014-11-25 12:09:35 +0100

    add new view for upgrading security level

---
+++ Digipost/IncreaseSecurityLevelViewController.swift
@@ -0,0 +1,21 @@
+//
+//  IncreaseSecurityLevelViewController.swift
+//  Digipost
+//
+//  Created by Håkon Bogen on 25/11/14.
+//  Copyright (c) 2014 Posten. All rights reserved.
+//
+
+import UIKit
+
+class IncreaseSecurityLevelViewController: UIViewController, UIWebViewDelegate {
+    
+    @IBOutlet weak var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
