33398fe76c7cf9cefb9f4f52d286c0947909f8da
Henrik Holmsen <henrik.holmsen@gmail.com>
2015-01-13 10:41:15 +0100

    Added account tableview cell

---
+++ Digipost/AccountTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AccountTableViewCell.swift
+//  Digipost
+//
+//  Created by Henrik Holmsen on 13.01.15.
+//  Copyright (c) 2015 Posten. All rights reserved.
+//
+
+import UIKit
+
+class AccountTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
