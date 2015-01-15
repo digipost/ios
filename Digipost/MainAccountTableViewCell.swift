b3a7543bb9f768874fdc39d348ad9091b63200fd
Hannes Waller <hkwaller@gmail.com>
2015-01-15 15:08:20 +0100

    adapt custom cells to new design

---
+++ Digipost/MainAccountTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  MainAccountTableViewCell.swift
+//  Digipost
+//
+//  Created by Hannes Waller on 2015-01-15.
+//  Copyright (c) 2015 Posten. All rights reserved.
+//
+
+import UIKit
+
+class MainAccountTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var accountImageView: UIImageView!
+    @IBOutlet weak var accountNameLabel: UILabel!
+    @IBOutlet weak var initialLabel: UILabel!
+    @IBOutlet weak var unreadMessages: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        accountImageView.backgroundColor = UIColor.grayColor()
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
