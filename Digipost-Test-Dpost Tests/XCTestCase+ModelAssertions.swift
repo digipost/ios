4c0c7a3516a99e59d02ceab19d1571bda7599c63
Håkon Bogen <hakon.bogen@shortcut.no>
2015-01-20 15:46:17 +0100

    add test classes for model & api

---
+++ Digipost-Test-Dpost Tests/XCTestCase+ModelAssertions.swift
@@ -0,0 +1,14 @@
+//
+//  XCTestCase+ModelAssertions.swift
+//  Digipost
+//
+//  Created by Håkon Bogen on 20/01/15.
+//  Copyright (c) 2015 Posten. All rights reserved.
+//
+
+import UIKit
+
+extension XCTestCase {
+    
+ 
+}
