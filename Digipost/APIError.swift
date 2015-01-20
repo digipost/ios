26e25f2d1e76e151cc4ffe161300f443005adfdb
Håkon Bogen <hakon.bogen@shortcut.no>
2015-01-20 12:15:21 +0100

    add an error that should be used to show meaningful errors to users

---
+++ Digipost/APIError.swift
@@ -0,0 +1,13 @@
+//
+//  APIError.swift
+//  Digipost
+//
+//  Created by Håkon Bogen on 20/01/15.
+//  Copyright (c) 2015 Posten. All rights reserved.
+//
+
+import UIKit
+
+class APIError: NSError {
+   
+}
