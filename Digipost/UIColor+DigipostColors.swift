9a65bef9a76c02f746d885a2a013da1eb246c0f1
Hannes Waller <hkwaller@gmail.com>
2015-01-20 15:07:51 +0100

    add digipost colours to UIColor file

--- Digipost/UIColor+DigipostColors.swift
+++ Digipost/UIColor+DigipostColors.swift
@@ -21,4 +21,21 @@ extension UIColor {
     class func digipostGreyOne () -> UIColor {
         return UIColor(r: 239, g: 66, b: 69)
     }
+    
+    class func digipostProfileViewBackground () -> UIColor {
+        return UIColor(r: 232, g: 232, b: 232)
+    }
+    
+    class func digipostProfileViewInitials () -> UIColor {
+        return UIColor(r: 141, g: 141, b: 141)
+    }
+    
+    class func digipostProfileTextColor () -> UIColor {
+        return UIColor(r: 77, g: 79, b: 83)
+    }
+    
+    class func digipostAccountViewBackground () -> UIColor {
+        return UIColor(r: 248, g: 248, b: 248)
+    }
+    
 }
