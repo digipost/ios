66b51989629620bc4741acf828eec9cd634221ad
Hannes Waller <hkwaller@gmail.com>
2015-01-22 13:13:21 +0100

    clean up code before merge

--- Digipost/MainAccountTableViewCell.swift
+++ Digipost/MainAccountTableViewCell.swift
@@ -21,17 +21,14 @@ class MainAccountTableViewCell: UITableViewCell {
         accountImageView.backgroundColor = UIColor.digipostProfileViewBackground()
         accountImageView.layer.cornerRadius = accountImageView.frame.width / 2
         accountImageView.clipsToBounds = true
+        
         accountNameLabel.textColor = UIColor.digipostProfileTextColor()
         initialLabel.textColor = UIColor.digipostProfileViewInitials()
         unreadMessages.textColor = UIColor.digipostProfileTextColor()
-        
-        
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
 }
