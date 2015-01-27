31c1e46fcb7a319a1be2a0bab33d9fe5b43c71b8
Henrik Holmsen <henrik.holmsen@gmail.com>
2015-01-27 13:08:27 +0100

    add onboarding storyboard with onboarding viewcontroller

---
+++ Digipost/OnboardingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  OnboardingViewController.swift
+//  Digipost
+//
+//  Created by Henrik Holmsen on 27.01.15.
+//  Copyright (c) 2015 Posten. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
