5442cc3e0030873c4fd49fc070acbd733a7de7da
Henrik Holmsen <henrik.holmsen@gmail.com>
2015-01-27 15:41:29 +0100

    add pagecontrol to scroll view + panorama image mockup

--- Digipost/OnboardingViewController.swift
+++ Digipost/OnboardingViewController.swift
@@ -8,12 +8,23 @@
 
 import UIKit
 
-class OnboardingViewController: UIViewController {
+class OnboardingViewController: UIViewController, UIScrollViewDelegate {
+
+    @IBOutlet var pageControl: UIPageControl!
+    @IBOutlet var scrollView: UIScrollView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        let panorama = UIImage(named: "panOslo")
+        let imageView = UIImageView(image: panorama)
+        
+        scrollView.contentSize = CGSizeMake(self.view.frame.width*5, self.view.frame.height)
+        scrollView.addSubview(imageView)
+        
+        pageControl.numberOfPages = 5
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +32,24 @@ class OnboardingViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        updatPageControl()
+    }
+    
+    func scrollViewWillBeginDecelerating(scrollView: UIScrollView) {
+    }
+    
+    func updatPageControl(){
+        // First, determine which page is currently visible
+        let pageWidth = scrollView.frame.size.width
+        let page = Int(floor((scrollView.contentOffset.x * 2.0 + pageWidth) / (pageWidth * 2.0)))
+        
+        pageControl.currentPage = page
+        
+        let pageX = scrollView.contentOffset.x - view.frame.width*CGFloat(page)
+        let prcnt = pageX / view.frame.width
+        println("x: \(pageX) percent: \(prcnt)")
+    }
 
     /*
     // MARK: - Navigation
