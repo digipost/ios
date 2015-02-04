924df519e3078a662f7900f316147dde2648bffa
Håkon Bogen <hakon.bogen@shortcut.no>
2015-02-04 09:41:47 +0100

    refactor some api methods into extension

---
+++ Digipost/APIClient+Document.swift
@@ -0,0 +1,14 @@
+//
+//  APIClient+Document.swift
+//  Digipost
+//
+//  Created by Håkon Bogen on 04/02/15.
+//  Copyright (c) 2015 Posten. All rights reserved.
+//
+
+import UIKit
+
+extension APIClient {
+    
+ 
+}
