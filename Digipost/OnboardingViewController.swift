4f032d4c332f363a7737442bbc626e98ee0ca82a
Henrik Holmsen <henrik.holmsen@gmail.com>
2015-02-14 08:51:50 +0100

    add forced orientation. Portrait for iPhone, landscape for iPad

--- Digipost/OnboardingViewController.swift
+++ Digipost/OnboardingViewController.swift
@@ -24,46 +24,41 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
     var secondAnimationView: LockView!
     var thirdAnimationView: ReceiptView!
     // ParallaxRates
-    let backgroundParallaxRate:CGFloat = 0.5
-    let mountainParallaxRate: CGFloat = 0.57
+    let backgroundParallaxRate:CGFloat = 0.5 // 0.4 for ipad
+    let mountainParallaxRate: CGFloat = 0.57 // 0.45 for ipad
     
     @IBOutlet var scrollView: UIScrollView!
     @IBOutlet var getStartedButton: UIButton!
     @IBOutlet var pageControll: UIPageControl!
     
-    var initialButtonPostionY:CGFloat = 0
+    var buttonInitialPostionY:CGFloat = 0
+    var buttonEndPositionY:CGFloat = 0
+    var logoInitialPositionY:CGFloat = 0
+    var welcomeLabelInitialPositionY:CGFloat = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        initialButtonPostionY = getStartedButton.center.y
-    }
-
     
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
-        
         let pageSize = view.frame.size
         let numOfPages:CGFloat = 5
         scrollView.delegate = self
         scrollView.contentSize = CGSizeMake(pageSize.width * numOfPages, pageSize.height)
-        
-        panBackground()
-      
-        println(bgImageView.frame)
-        println(bgMaskImageView.frame)
-        println(bgParallaxImageView.frame)
-     
+    }
+
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
         // Setup animation views
         setupAnimationViews()
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        buttonInitialPostionY = getStartedButton.center.y
+        logoInitialPositionY = logoImageView.center.y
+        welcomeLabelInitialPositionY = welcomeLabel.center.y
         panBackground()
     }
 
-    
     func setupAnimationViews(){
         
         let pageCenter = scrollView.frame.midY - (view.frame.width/4)
@@ -118,8 +113,8 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
         switch progress{
         case -1.0...1.0:
             firstAnimationView.progress =  progress
-            logoImageView.center.y = CGFloat(abs(Int32(scrollView.contentOffset.x + ((scrollView.frame.height / 2 ))))) - logoImageView.frame.height
-            welcomeLabel.center.y = logoImageView.center.y + welcomeLabel.frame.height/3
+            logoImageView.center.y = logoInitialPositionY + scrollView.contentOffset.x
+            welcomeLabel.center.y = welcomeLabelInitialPositionY + scrollView.contentOffset.x
             logoImageView.alpha = 1 - progress
             logoImageView.hidden = false
             welcomeLabel.hidden = false
@@ -129,13 +124,25 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
         case 2.0...3.0:
             thirdAnimationView.progress =  progress - 2
             secondAnimationView.progress = 3 - progress
-        case 3.0...5.0:
+        case 3.0...4.0:
             thirdAnimationView.progress = 4 - progress
-            getStartedButton.frame.origin.y = (scrollView.frame.width*4.2 - scrollView.contentOffset.x)
-            //pageControll.frame.origin.y = getStartedButton.frame.origin.y - getStartedButton.frame.height
-            pageControll.alpha = (4 - progress)*3
+            // get a startpoint where x is zero when on page 4, increasing the content offsett will give value from 0 to the size of one frame.width
+            let startPoint = scrollView.contentOffset.x - (scrollView.frame.width * 3)
+            // Then we use this translated x offset to increase/decreace the y position of the button
+            let translatedXPostitionToY = buttonInitialPostionY - startPoint
+            // stop decreasing y when welcome label reaches the middle of the screen
+            if startPoint < scrollView.frame.height / 2 {
+                getStartedButton.center.y =  translatedXPostitionToY
+                buttonEndPositionY = getStartedButton.center.y
+            }
+            pageControll.alpha = (4 - progress)
             logoImageView.hidden = true
             welcomeLabel.hidden = true
+        case 4.0...5.0:
+            // get a startpoint where x is zero when on page 4
+            let startPoint = scrollView.contentOffset.x - (scrollView.frame.width * 4)
+            // If the user over scrolls the scrollview in x, move the button equally in y
+            getStartedButton.center.y = buttonEndPositionY - startPoint
         default: break
         }
     }
@@ -145,6 +152,19 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
         bgImageView.frame.origin.x = translatedOffsetX * backgroundParallaxRate
         bgMaskImageView.frame.origin.x = bgImageView.frame.origin.x
         bgParallaxImageView.frame.origin.x = translatedOffsetX * mountainParallaxRate
+    }
+    
+    override func shouldAutorotate() -> Bool {
+        return false
+    }
+    
+    override func supportedInterfaceOrientations() -> Int {
+        let device = UIDevice.currentDevice().userInterfaceIdiom
+        switch device{
+        case .Phone: return UIInterfaceOrientation.Portrait.rawValue
+        case .Pad: return UIInterfaceOrientation.LandscapeLeft.rawValue
+        default: return UIInterfaceOrientation.Portrait.rawValue
+        }
     }
     
 }
