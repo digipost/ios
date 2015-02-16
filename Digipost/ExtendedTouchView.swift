6340334a4b1f159f2078771627e5060d53fb437d
Henrik Holmsen <henrik.holmsen@gmail.com>
2015-02-16 14:02:57 +0100

    add extended touch view for transferring all events to scrollview

---
+++ Digipost/ExtendedTouchView.swift
@@ -0,0 +1,21 @@
+//
+//  ExtendedTouchView.swift
+//  Digipost
+//
+//  Created by Henrik Holmsen on 16.02.15.
+//  Copyright (c) 2015 Posten. All rights reserved.
+//
+
+import UIKit
+
+class ExtendedTouchView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
