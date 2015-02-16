8231351f629799e2e3263cfe2ccabdcd9a9d6ab0
Henrik Holmsen <henrik.holmsen@gmail.com>
2015-02-16 11:39:19 +0100

    add mock up views for animtaion views and labels

--- Digipost/OnboardingViewController.swift
+++ Digipost/OnboardingViewController.swift
@@ -23,9 +23,8 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
     var firstAnimationView: DeviceView!
     var secondAnimationView: LockView!
     var thirdAnimationView: ReceiptView!
-    // ParallaxRates
-    let backgroundParallaxRate:CGFloat = 0.5 // 0.4 for ipad
-    let mountainParallaxRate: CGFloat = 0.57 // 0.45 for ipad
+    @IBOutlet var animationMockView: UIView!
+    @IBOutlet var animationMockLabel: UILabel!
     
     @IBOutlet var scrollView: UIScrollView!
     @IBOutlet var getStartedButton: UIButton!
@@ -36,6 +35,10 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
     var logoInitialPositionY:CGFloat = 0
     var welcomeLabelInitialPositionY:CGFloat = 0
     
+    // ParallaxRates
+    var backgroundParallaxSpeed:CGFloat!
+    var mountainParallaxSpeed: CGFloat!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     
@@ -43,6 +46,20 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
         let numOfPages:CGFloat = 5
         scrollView.delegate = self
         scrollView.contentSize = CGSizeMake(pageSize.width * numOfPages, pageSize.height)
+        
+        // Set parallax speed depending on device
+        let device = UIDevice.currentDevice().userInterfaceIdiom
+        switch device {
+        case .Phone:
+            backgroundParallaxSpeed = 0.5
+            mountainParallaxSpeed = 0.57
+            println("iPhone")
+        case .Pad:
+            backgroundParallaxSpeed = 0.1
+            mountainParallaxSpeed = 0.13
+            println("iPad")
+        default: break
+        }
     }
 
     override func viewDidAppear(animated: Bool) {
@@ -53,50 +70,53 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        
         buttonInitialPostionY = getStartedButton.center.y
         logoInitialPositionY = logoImageView.center.y
         welcomeLabelInitialPositionY = welcomeLabel.center.y
         panBackground()
     }
 
-    func setupAnimationViews(){
+    func setupAnimationViews() {
         
-        let pageCenter = scrollView.frame.midY - (view.frame.width/4)
+        let pageCenter = animationMockView.center
         let viewOffset = scrollView.frame.width
-        let viewSize = CGSize(width: scrollView.frame.width, height: scrollView.frame.height)
+        let viewSize = animationMockView.frame.size
         
-        firstAnimationView = DeviceView(frame: CGRectMake(viewOffset, 0, viewSize.width, viewSize.height))
+        firstAnimationView = DeviceView(frame: animationMockView.frame)
+        firstAnimationView.center.x = animationMockView.center.x + viewOffset
         scrollView.addSubview(firstAnimationView!)
         
-        let firstAnimationText = UILabel(frame: CGRectMake(firstAnimationView.frame.origin.x, firstAnimationView.frame.origin.y + viewSize.width/3 , viewSize.width/2, viewSize.height/2))
-        firstAnimationText.center = CGPointMake(firstAnimationView.center.x, firstAnimationText.center.y)
+        let firstAnimationText = UILabel(frame: animationMockLabel.frame)
+        firstAnimationText.center.x = animationMockLabel.center.x + viewOffset
         firstAnimationText.textColor = UIColor.blackColor()
         firstAnimationText.textAlignment = .Center
         firstAnimationText.numberOfLines = 2
-        firstAnimationText.text = "Ha med deg\npostkassen din overalt"
+        firstAnimationText.text = "Ha med deg\npostkassen din overalt" // TODO: Localization
         scrollView.addSubview(firstAnimationText)
         
-        secondAnimationView = LockView(frame: CGRectMake(viewOffset*2, 0, viewSize.width, viewSize.height))
+        secondAnimationView = LockView(frame: animationMockView.frame)
+        secondAnimationView.center.x = animationMockView.center.x + viewOffset * 2
         scrollView.addSubview(secondAnimationView!)
-        secondAnimationView.constraints()
         
-        let secondAnimationText = UILabel(frame: CGRectMake(secondAnimationView.frame.origin.x, secondAnimationView.frame.origin.y + viewSize.width/3 , viewSize.width/2, viewSize.height/2))
-        secondAnimationText.center = CGPointMake(secondAnimationView.center.x, secondAnimationText.center.y)
+        let secondAnimationText = UILabel(frame: animationMockLabel.frame)
+        secondAnimationText.center.x = animationMockLabel.center.x + viewOffset * 2
         secondAnimationText.textColor = UIColor.blackColor()
         secondAnimationText.textAlignment = .Center
         secondAnimationText.numberOfLines = 2
-        secondAnimationText.text = "Trygg oppbevaring\nav viktige dokumenter"
+        secondAnimationText.text = "Trygg oppbevaring\nav viktige dokumenter" // TODO: Localization
         scrollView.addSubview(secondAnimationText)
         
-        thirdAnimationView = ReceiptView(frame: CGRectMake(viewOffset*3, 0, viewSize.width, viewSize.height))
+        thirdAnimationView = ReceiptView(frame: animationMockView.frame)
+        thirdAnimationView.center.x = animationMockView.center.x + viewOffset * 3
         scrollView.addSubview(thirdAnimationView!)
         
-        let thirdAnimationText = UILabel(frame: CGRectMake(thirdAnimationView.frame.origin.x, thirdAnimationView.frame.origin.y + viewSize.width/3 , viewSize.width/2, viewSize.height/2))
-        thirdAnimationText.center = CGPointMake(thirdAnimationView.center.x, thirdAnimationText.center.y)
+        let thirdAnimationText = UILabel(frame: animationMockLabel.frame)
+        thirdAnimationText.center.x = animationMockLabel.center.x + viewOffset * 3
         thirdAnimationText.textColor = UIColor.blackColor()
         thirdAnimationText.textAlignment = .Center
         thirdAnimationText.numberOfLines = 2
-        thirdAnimationText.text = "Full kontroll med\nelektroniske kvittering"
+        thirdAnimationText.text = "Full kontroll med\nelektroniske kvitteringer" // TODO: Localization
         scrollView.addSubview(thirdAnimationText)
     }
     
@@ -106,11 +126,10 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
         pageControll.currentPage = scrollView.currentPage
     }
     
-    func updateAnimationViewProgress(){
+    func updateAnimationViewProgress() {
         
         let progress = scrollView.pageProgressInPercentage
-        
-        switch progress{
+        switch progress {
         case -1.0...1.0:
             firstAnimationView.progress =  progress
             logoImageView.center.y = logoInitialPositionY + scrollView.contentOffset.x
@@ -127,11 +146,11 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
         case 3.0...4.0:
             thirdAnimationView.progress = 4 - progress
             // get a startpoint where x is zero when on page 4, increasing the content offsett will give value from 0 to the size of one frame.width
-            let startPoint = scrollView.contentOffset.x - (scrollView.frame.width * 3)
+            let startPointX = scrollView.contentOffset.x - (scrollView.frame.width * 3)
             // Then we use this translated x offset to increase/decreace the y position of the button
-            let translatedXPostitionToY = buttonInitialPostionY - startPoint
+            let translatedXPostitionToY = buttonInitialPostionY - startPointX
             // stop decreasing y when welcome label reaches the middle of the screen
-            if startPoint < scrollView.frame.height / 2 {
+            if startPointX < scrollView.frame.height / 2 {
                 getStartedButton.center.y =  translatedXPostitionToY
                 buttonEndPositionY = getStartedButton.center.y
             }
@@ -139,33 +158,34 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
             logoImageView.hidden = true
             welcomeLabel.hidden = true
         case 4.0...5.0:
-            // get a startpoint where x is zero when on page 4
-            let startPoint = scrollView.contentOffset.x - (scrollView.frame.width * 4)
+            // get a startpoint where x is zero when on page 5
+            let startPointX = scrollView.contentOffset.x - (scrollView.frame.width * 4)
             // If the user over scrolls the scrollview in x, move the button equally in y
-            getStartedButton.center.y = buttonEndPositionY - startPoint
+            getStartedButton.center.y = buttonEndPositionY - startPointX
         default: break
         }
     }
     
-    func panBackground(){
+    func panBackground() {
         let translatedOffsetX = -(scrollView.contentOffset.x + scrollView.scrollableEdgeOffset)
-        bgImageView.frame.origin.x = translatedOffsetX * backgroundParallaxRate
+        bgImageView.frame.origin.x = translatedOffsetX * backgroundParallaxSpeed
         bgMaskImageView.frame.origin.x = bgImageView.frame.origin.x
-        bgParallaxImageView.frame.origin.x = translatedOffsetX * mountainParallaxRate
+        bgParallaxImageView.frame.origin.x = translatedOffsetX * mountainParallaxSpeed
     }
     
-    override func shouldAutorotate() -> Bool {
-        return false
-    }
-    
-    override func supportedInterfaceOrientations() -> Int {
-        let device = UIDevice.currentDevice().userInterfaceIdiom
-        switch device{
-        case .Phone: return UIInterfaceOrientation.Portrait.rawValue
-        case .Pad: return UIInterfaceOrientation.LandscapeLeft.rawValue
-        default: return UIInterfaceOrientation.Portrait.rawValue
-        }
-    }
+//    override func shouldAutorotate() -> Bool {
+//        return false
+//    }
+//    
+//    override func supportedInterfaceOrientations() -> Int {
+//        
+//        let device = UIDevice.currentDevice().userInterfaceIdiom
+//        switch device {
+//        case .Phone: return UIInterfaceOrientation.Portrait.rawValue
+//        case .Pad: return UIInterfaceOrientation.LandscapeRight.rawValue
+//        default: return UIInterfaceOrientation.Portrait.rawValue
+//        }
+//    }
     
 }
 
