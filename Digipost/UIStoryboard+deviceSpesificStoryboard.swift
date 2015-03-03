1c705211036189ab24cb5a04a63aabd97860929b
Henrik Holmsen <henrik.holmsen@gmail.com>
2015-03-03 14:53:26 +0100

    add UIStoryboard extension for current device storyboard and its project reference

---
+++ Digipost/UIStoryboard+deviceSpesificStoryboard.swift
@@ -0,0 +1,23 @@
+//
+//  UIStoryboard+deviceSpesificStoryboard.swift
+//  Digipost
+//
+//  Created by Henrik Holmsen on 03.03.15.
+//  Copyright (c) 2015 Posten. All rights reserved.
+//
+
+import Foundation
+
+extension UIStoryboard {
+    class func storyboardForCurrentUserInterfaceIdiom() -> UIStoryboard {
+        let device = UIDevice.currentDevice().userInterfaceIdiom
+        switch device {
+        case .Phone:
+            return UIStoryboard(name: "Main_iPhone", bundle: nil) // TODO: Move storyboard name to constants
+        case .Pad:
+            return UIStoryboard(name: "Main_iPad", bundle: nil)
+        case .Unspecified:
+            return UIStoryboard(name: "Main_iPhone", bundle: nil)
+        }
+    }
+}
