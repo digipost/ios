c2b4dd5c61c14ec57948f9b73c66e524f4f29b12
Henrik Holmsen <henrik.holmsen@gmail.com>
2015-03-04 14:20:14 +0100

    add onboardingLoginViewController content for presenting POSLoginViewController

--- Digipost/OnboardingLoginViewController.swift
+++ Digipost/OnboardingLoginViewController.swift
@@ -6,8 +6,36 @@
 //  Copyright (c) 2015 Posten. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
 class OnboardingLoginViewController: UIViewController {
 
+    @IBAction func loginButtonAction(sender: UIButton) {
+                var storyboard:UIStoryboard!
+                let userDefaults = NSUserDefaults.standardUserDefaults()
+        
+                let shouldViewNewFeatures = !userDefaults.boolForKey("hasViewedNewFeaturesForVersion")
+        
+                if shouldViewNewFeatures{
+                    storyboard = UIStoryboard(name: "NewFeatures", bundle: nil)
+                } else {
+                    storyboard = UIStoryboard.storyboardForCurrentUserInterfaceIdiom()
+                }
+        
+                // Store that user has viewed the onboarding
+                let userdefaults = NSUserDefaults.standardUserDefaults()
+                userDefaults.setBool(true, forKey: "hasViewedOnboarding")
+                userDefaults.synchronize()
+        
+                let viewcontroller:UIViewController = storyboard.instantiateInitialViewController() as UIViewController
+                self.presentViewController(viewcontroller, animated: false) { () -> Void in
+                }
+    }
+    
+    @IBAction func newUserButtonAction(sender: UIButton) {
+    }
+    
+    @IBAction func privacyButtonAction(sender: UIButton) {
+    }
+    
 }
