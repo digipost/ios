11ceac5adb9e5df21934f03bcbd87d6a67ba3f80
Henrik Holmsen <henrik.holmsen@gmail.com>
2015-03-04 14:18:18 +0100

    add onboardingLoginViewController project reference

---
+++ Digipost/OnboardingLoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  OnboardingLoginViewController.swift
+//  Digipost
+//
+//  Created by Henrik Holmsen on 04.03.15.
+//  Copyright (c) 2015 Posten. All rights reserved.
+//
+
+import Cocoa
+
+class OnboardingLoginViewController: UIViewController {
+
+}
