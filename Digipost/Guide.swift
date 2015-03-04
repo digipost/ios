0bce4b7706975be2264bdf3c40dde215340aabe2
Håkon Bogen <hakon.bogen@shortcut.no>
2015-03-04 15:34:04 +0100

    add scaffolding & test items for guides

---
+++ Digipost/Guide.swift
@@ -0,0 +1,9 @@
+//
+//  Guide.swift
+//  Digipost
+//
+//  Created by Håkon Bogen on 04/03/15.
+//  Copyright (c) 2015 Posten. All rights reserved.
+//
+
+import Foundation
---
+++ Digipost/GuideItem.swift
@@ -0,0 +1,13 @@
+//
+//  GuideItem.swift
+//  Digipost
+//
+//  Created by Håkon Bogen on 04/03/15.
+//  Copyright (c) 2015 Posten. All rights reserved.
+//
+
+import UIKit
+
+class GuideItem: NSObject {
+   
+}
