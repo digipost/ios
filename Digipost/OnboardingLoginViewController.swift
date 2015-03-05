cbd168b6becd6fc5573762ec2eef8435b29e3961
Henrik Holmsen <henrik.holmsen@gmail.com>
2015-03-05 10:55:40 +0100

    remove direct fowarding to new features when loging in

--- Digipost/OnboardingLoginViewController.swift
+++ Digipost/OnboardingLoginViewController.swift
@@ -11,25 +11,14 @@ import UIKit
 class OnboardingLoginViewController: UIViewController {
 
     @IBAction func loginButtonAction(sender: UIButton) {
-                var storyboard:UIStoryboard!
-                let userDefaults = NSUserDefaults.standardUserDefaults()
-        
-                let shouldViewNewFeatures = !userDefaults.boolForKey("hasViewedNewFeaturesForVersion")
-        
-                if shouldViewNewFeatures{
-                    storyboard = UIStoryboard(name: "NewFeatures", bundle: nil)
-                } else {
-                    storyboard = UIStoryboard.storyboardForCurrentUserInterfaceIdiom()
-                }
-        
-                // Store that user has viewed the onboarding
-                let userdefaults = NSUserDefaults.standardUserDefaults()
-                userDefaults.setBool(true, forKey: "hasViewedOnboarding")
-                userDefaults.synchronize()
-        
-                let viewcontroller:UIViewController = storyboard.instantiateInitialViewController() as UIViewController
-                self.presentViewController(viewcontroller, animated: false) { () -> Void in
-                }
+
+        // Store that user has viewed the onboarding
+        let userdefaults = NSUserDefaults.standardUserDefaults()
+        userDefaults.setBool(true, forKey: "hasViewedOnboarding")
+        userDefaults.synchronize()
+        let storyboard = UIStoryboard.storyboardForCurrentUserInterfaceIdiom()
+        let viewcontroller:UIViewController = storyboard.instantiateInitialViewController() as UIViewController
+        self.presentViewController(viewcontroller, animated: false) { () -> Void in
     }
     
     @IBAction func newUserButtonAction(sender: UIButton) {
