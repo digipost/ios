2a9d9b6858667f2dc4b5b775929a89eedb2c6c82
Hannes Waller <hkwaller@gmail.com>
2015-03-05 11:58:56 +0100

    fix onboarding viewcontroller

--- Digipost/OnboardingLoginViewController.swift
+++ Digipost/OnboardingLoginViewController.swift
@@ -13,12 +13,12 @@ class OnboardingLoginViewController: UIViewController {
     @IBAction func loginButtonAction(sender: UIButton) {
 
         // Store that user has viewed the onboarding
-        let userdefaults = NSUserDefaults.standardUserDefaults()
+        let userDefaults = NSUserDefaults.standardUserDefaults()
         userDefaults.setBool(true, forKey: "hasViewedOnboarding")
         userDefaults.synchronize()
         let storyboard = UIStoryboard.storyboardForCurrentUserInterfaceIdiom()
         let viewcontroller:UIViewController = storyboard.instantiateInitialViewController() as UIViewController
-        self.presentViewController(viewcontroller, animated: false) { () -> Void in
+        self.presentViewController(viewcontroller, animated: false, completion: nil)
     }
     
     @IBAction func newUserButtonAction(sender: UIButton) {
