55eeeefec5e37447a048c4b5aa3a871da5e1a95b
Henrik Holmsen <henrik.holmsen@gmail.com>
2015-04-08 10:01:30 +0200

    add view controller classes for composer storyboard

---
+++ Digipost/ComposerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ComposerViewController.swift
+//  Digipost
+//
+//  Created by Henrik Holmsen on 08.04.15.
+//  Copyright (c) 2015 Posten. All rights reserved.
+//
+
+import UIKit
+
+class ComposerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
