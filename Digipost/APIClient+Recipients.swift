a268ff1c486fc51a2776894e87a724d238e4939b
Hannes Waller <hkwaller@gmail.com>
2015-04-08 14:54:01 +0200

    add extension to APIClient for recipients

--- Digipost/APIClient+Recipients.swift
+++ Digipost/APIClient+Recipients.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+extension APIClient {
+
+    func getRecipients(searchString: String, success: (Dictionary<String,AnyObject>) -> Void , failure: (error: APIError) -> ()) {
+        let encodedSearchString:String =  searchString.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.LiteralSearch, range: nil)
+        let safeString = encodedSearchString.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)
+        let urlString = "https://www.digipost.no/post/api/recipients/search?recipientId=\(safeString!)"
+        println(safeString)
+        let task = urlSessionJSONTask(url: urlString, success: success) { (error) -> () in
+            if error.code == Constants.Error.Code.oAuthUnathorized {
+                self.getRecipients(urlString, success: success, failure: failure)
+            } else {
+                failure(error: error)
+            }
+        }
+        validateTokensThenPerformTask(task!)
+    }
+
+
+}
