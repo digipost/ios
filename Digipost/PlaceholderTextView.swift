d935538c86255a8784ede51782768923553277e3
Henrik Holmsen <henrik.holmsen@gmail.com>
2015-04-08 15:45:44 +0200

    add begining of placerhoder text view

---
+++ Digipost/PlaceholderTextView.swift
@@ -0,0 +1,22 @@
+//
+//  PlaceholderTextView.swift
+//  Digipost
+//
+//  Created by Henrik Holmsen on 08.04.15.
+//  Copyright (c) 2015 Posten. All rights reserved.
+//
+
+import UIKit
+
+class PlaceholderTextView: UITextView {
+
+    
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
