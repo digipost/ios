5ba7d7648216f3f407a82afa90e4c49c65708edf
Henrik Holmsen <henrik.holmsen@gmail.com>
2015-04-09 13:24:08 +0200

    add image resize extension and implement delegate methods for module selector delegate

--- Digipost/ComposerViewController.swift
+++ Digipost/ComposerViewController.swift
@@ -8,7 +8,23 @@
 
 import UIKit
 
-class ComposerViewController: UIViewController {
+extension UIImage{
+    
+    func resize(toSize size: CGSize, completionHandler: (resizedImage: UIImage, data: NSData) ->() ){
+        dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0), { () -> Void in
+            UIGraphicsBeginImageContextWithOptions(size, false, 1.0)
+            self.drawInRect(CGRectMake(0, 0, size.width, size.height))
+            let resizedImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            let imageData = UIImagePNGRepresentation(resizedImage)
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                completionHandler(resizedImage: resizedImage, data: imageData)
+            })
+        })
+    }
+}
+
+class ComposerViewController: UIViewController, ModuleSelectorViewControllerDelegate {
 
     @IBOutlet var tableView: UITableView!
     var tableViewDataSource: ComposerTableViewDataSource?
@@ -16,9 +32,7 @@ class ComposerViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setupTableView()
-
     }
     
     // MARK: - TableView Setup
@@ -33,21 +47,46 @@ class ComposerViewController: UIViewController {
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 44
     }
+    
+    // MARK: - ModuleSelectorViewController Delegate
+    
+    func moduleSelectorViewController(moduleSelectorViewController: ModuleSelectorViewController, didtSelectModule module: ComposerModule) {
+        
+        switch module.type{
+            
+        case .ImageModule:
+            
+            let squareSize = CGSizeMake(tableView.frame.width, tableView.frame.width)
+            module.image?.resize(toSize: squareSize, completionHandler: { (resizedImage, data) -> () in
+                module.image = resizedImage
+            })
+            
+        case .TextModule:
+            println()
+        }
+        
+        tableViewDataSource?.tableData.append(module)
+        moduleSelectorViewController.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func moduleSelectorViewControllerWasDismissed(moduleSelectorViewController: ModuleSelectorViewController) {
+        moduleSelectorViewController.dismissViewControllerAnimated(true, completion: nil)
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        
+        if segue.destinationViewController.isKindOfClass(ModuleSelectorViewController){
+            let moduleSelectViewController = segue.destinationViewController as ModuleSelectorViewController
+            moduleSelectViewController.delegate = self
+        }
     }
-    */
+    
 
 }
