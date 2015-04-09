f13c5bfccd2b97722a4372492ff9bb1cb3b5978e
Henrik Holmsen <henrik.holmsen@gmail.com>
2015-04-09 08:52:38 +0200

    begin implementation of custom text view with placeholder

--- Digipost/PlaceholderTextView.swift
+++ Digipost/PlaceholderTextView.swift
@@ -9,7 +9,37 @@
 import UIKit
 
 class PlaceholderTextView: UITextView {
+    
+    var placeholderLabel: UILabel = UILabel()
+    var placeholderColor: UIColor = UIColor.lightGrayColor()
+    var placeholderText: String = ""
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(frame: CGRect, withPlaceholderText placeholderText: String){
+        self.placeholderText = placeholderText
+        let textContainer = NSTextContainer(size: frame.size)
+        super.init(frame: frame, textContainer: textContainer)
+        
+        placeholderLabel.frame = frame
+        placeholderLabel.backgroundColor = UIColor.clearColor()
+        placeholderLabel.text = placeholderText
+        self.addSubview(placeholderLabel)
+        self.sendSubviewToBack(placeholderLabel)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "textDidChange:", name: UITextViewTextDidChangeNotification, object: nil)
+    }
+    
+    deinit{
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
 
+    
+    func textDidChange(notification:NSNotification?) -> (Void) {
+        println("Text changed")
+    }
     
     /*
     // Only override drawRect: if you perform custom drawing.
