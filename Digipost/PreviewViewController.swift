3322c7e97ee2de75f322b5c01a7ac56fb6f27305
Hannes Waller <hkwaller@gmail.com>
2015-04-15 12:09:02 +0200

    add tableview to previewviewcontroller

--- Digipost/PreviewViewController.swift
+++ Digipost/PreviewViewController.swift
@@ -30,31 +30,16 @@ class PreviewViewController: UIViewController {
                     let imageModule = module as? ComposerImageModule
                     println("ImageModule")
                 }
-                
             }
         }
     }
     
+    @IBOutlet weak var recipientsLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
+    
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    
-    /*
-    // MARK: - Navigation
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    // Get the new view controller using segue.destinationViewController.
-    // Pass the selected object to the new view controller.
-    }
-    */
     
 }
