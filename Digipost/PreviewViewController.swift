5c3c7f96ef7a6b79d64bba843d19a23bf78b1405
Hannes Waller <hkwaller@gmail.com>
2015-04-15 13:17:58 +0200

    readd files to project

--- Digipost/PreviewViewController.swift
+++ Digipost/PreviewViewController.swift
@@ -11,28 +11,7 @@ import UIKit
 class PreviewViewController: UIViewController {
     
     var recipients = [Recipient]()
-    var modules: [ComposerModule] = [ComposerModule](){
-        
-        didSet{
-            println("........................................")
-            println("           Composer Contents:          ")
-            println("........................................")
-            
-            for (index,module) in enumerate(modules) {
-                
-                switch module.type{
-                    
-                case .TextModule:
-                    let textModule = module as? ComposerTextModule
-                    let text = textModule?.text
-                    println("TextModule: \(text!)")
-                case .ImageModule:
-                    let imageModule = module as? ComposerImageModule
-                    println("ImageModule")
-                }
-            }
-        }
-    }
+    var modules: [ComposerModule] = [ComposerModule]()
     
     @IBOutlet weak var recipientsLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
