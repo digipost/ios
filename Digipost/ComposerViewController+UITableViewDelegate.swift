82805572d6eed9c1e16c961256ce4e3d94678954
Håkon Bogen <hakon.bogen@shortcut.no>
2015-04-15 13:01:33 +0200

    add dynamic sizing when editing textviews in composer

--- Digipost/ComposerViewController+UITableViewDelegate.swift
+++ Digipost/ComposerViewController+UITableViewDelegate.swift
@@ -8,21 +8,33 @@
 
 import UIKit
 
+
+struct ComposerViewControllerDelegateConstants {
+    static let textViewMarginTop : CGFloat = 5
+    static let textViewMarginBottom : CGFloat = 5
+
+    static let minimumCellSize : CGFloat = 44
+
+}
+
 extension ComposerViewController {
 
     
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        
+        let module = self.tableViewDataSource.composerModule(atIndexPath: indexPath)
+        if let textModule = module as? TextComposerModule {
+            return height(textComposerModule: textModule)
+        }
         return 44
     }
 
-
-    func height(forTextInCellTextView text: String) -> CGFloat {
-        if let textView = exampleTextView() {
-            textViewWidth = notesTextView!.frame.size.width
-            textView = notesTextView!
-        } else {
-
-        }
+    func height(#textComposerModule: TextComposerModule) -> CGFloat {
+        let textView = UITextView()
+        textView.text = textComposerModule.text
+        textView.font = textComposerModule.font
+        textView.frame.size.width = self.tableView.frame.size.width - 40 // TODO: use the actual margin!
+        let size = textView.sizeThatFits(CGSizeMake(textView.frame.size.width, 1000))
+        let calculatedHeight = size.height + ComposerViewControllerDelegateConstants.textViewMarginBottom
+        return max(calculatedHeight, ComposerViewControllerDelegateConstants.minimumCellSize)
     }
 }
