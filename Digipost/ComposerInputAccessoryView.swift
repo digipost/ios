7b5e019e2a89fab1c32172e4772ba2df5a593fe3
Hannes Waller <hkwaller@gmail.com>
2015-04-16 15:52:47 +0200

    add nib and controller for inputaccessory view

---
+++ Digipost/ComposerInputAccessoryView.swift
@@ -0,0 +1,21 @@
+//
+//  ComposerInputAccessoryView.swift
+//  Digipost
+//
+//  Created by Hannes Waller on 2015-04-16.
+//  Copyright (c) 2015 Posten. All rights reserved.
+//
+
+import UIKit
+
+class ComposerInputAccessoryView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
