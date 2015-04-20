f12140b733741bcac98976e9518ca15e22a2f189
Hannes Waller <hkwaller@gmail.com>
2015-04-20 09:13:44 +0200

    add alignment to accessoryview

--- Digipost/ComposerInputAccessoryView.swift
+++ Digipost/ComposerInputAccessoryView.swift
@@ -14,19 +14,32 @@ class ComposerInputAccessoryView: UIView {
     @IBOutlet weak var textView: UITextView!
     
     override func awakeFromNib() {
-        super.awakeFromNib()
-        
+        super.awakeFromNib()        
     }
     
     @IBAction func alignContentLeft(sender: AnyObject) {
-        println(textView.text)
+        textView.textAlignment = .Left
     }
     
     @IBAction func alignContentCenter(sender: AnyObject) {
-
+        textView.textAlignment = .Center
     }
 
     @IBAction func alignContentRight(sender: AnyObject) {
-
+        textView.textAlignment = .Right
+    }
+    
+    func setLabel() {
+        let font = textView.font
+        switch font {
+        case UIFont.preferredFontForTextStyle(UIFontTextStyleHeadline):
+            nameLabel.text = "Headline"
+        case UIFont.preferredFontForTextStyle(UIFontTextStyleBody):
+            nameLabel.text = "Body"
+        case UIFont.preferredFontForTextStyle(UIFontTextStyleSubheadline):
+            nameLabel.text = "Subheader"
+        default:
+            nameLabel.text = "Boaaady"
+        }
     }
 }
