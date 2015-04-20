e558e82f108a949ec8199499ab312d7bae96db3d
Hannes Waller <hkwaller@gmail.com>
2015-04-20 12:35:29 +0200

    add recipient tableview cell

---
+++ Digipost/RecipientTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  RecipientTableViewCell.swift
+//  Digipost
+//
+//  Created by Hannes Waller on 2015-04-20.
+//  Copyright (c) 2015 Posten. All rights reserved.
+//
+
+import UIKit
+
+class RecipientTableViewCell: UITableViewCell {
+    @IBOutlet weak var initialsLabel: UILabel!
+    @IBOutlet weak var initialsView: UIView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var addedButton: UIButton!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        initialsView.layer.cornerRadius = 50
+        addedButton.layer.cornerRadius = 50
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    func loadCell(name: String, address: String) {
+        initialsLabel.text = name.initials()
+        nameLabel.text = name
+        addressLabel.text = address
+        addedButton.hidden = true
+    }
+}
