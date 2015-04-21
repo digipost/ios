52a50fe14395ebdbad056830fcb2c2d459c0736f
Hannes Waller <hkwaller@gmail.com>
2015-04-21 12:03:40 +0200

    add delegate in recipients view controller

--- Digipost/RecipientsViewController.swift
+++ Digipost/RecipientsViewController.swift
@@ -8,16 +8,18 @@
 
 import UIKit
 
+protocol RecipientsViewControllerDelegate {
+    func addRecipients(recipients: [Recipient])
+}
+
 class RecipientViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var saveBarButtonItem: UIBarButtonItem!
     
-    var recipients : [Recipient] = [Recipient]() {
-        didSet { tableView.reloadData() }
-    }
-    var addedRecipients : [Recipient] = [Recipient]() {
-        didSet { tableView.reloadData() }
-    }
+    var delegate: RecipientsViewControllerDelegate? = nil
+    
+    var recipients : [Recipient] = [Recipient]()
+    var addedRecipients : [Recipient] = [Recipient]()
     
     var recipientSearchController = UISearchController()
 
@@ -46,49 +48,48 @@ class RecipientViewController: UIViewController {
             return controller
         })()
     }
-
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "recipientsAddedSegue" {
-            if let composerController = segue.destinationViewController as? ComposerViewController {
-                composerController.recipients = addedRecipients
-            }
-        }
-    }
     
     override func viewWillDisappear(animated: Bool) {
-        super.viewWillDisappear(false)
+        super.viewWillDisappear(animated)
         recipientSearchController.active = false
         recipientSearchController.searchBar.frame = CGRectMake(0, 0, 0, 0)
     }
     
     override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(false)
+        super.viewWillAppear(animated)
         recipientSearchController.searchBar.frame = self.navigationController!.navigationBar.frame
     }
- 
-    @IBAction func dismissViewController(sender: AnyObject) {
-        
-        let alertController = UIAlertController(title: "Brev lukkes", message: "Vil du lagre utkastet før du avslutter?", preferredStyle: UIAlertControllerStyle.Alert)
-        let saveDraftAction = UIAlertAction(title: "Lagre utkast",
-            style: UIAlertActionStyle.Default)
-            { [unowned self, alertController] (action: UIAlertAction!) -> Void in
-            println("Saved")
-                self.navigationController?.dismissViewControllerAnimated(true, completion: { () -> Void in
-                })
+// 
+//    @IBAction func dismissViewController(sender: AnyObject) {
+//        
+//        let alertController = UIAlertController(title: "Brev lukkes", message: "Vil du lagre utkastet før du avslutter?", preferredStyle: UIAlertControllerStyle.Alert)
+//        let saveDraftAction = UIAlertAction(title: "Lagre utkast",
+//            style: UIAlertActionStyle.Default)
+//            { [unowned self, alertController] (action: UIAlertAction!) -> Void in
+//            println("Saved")
+//                self.navigationController?.dismissViewControllerAnimated(true, completion: { () -> Void in
+//                })
+//        }
+//        
+//        let quitAction = UIAlertAction(title: "Lukk",
+//            style: UIAlertActionStyle.Destructive)
+//            { [unowned self, alertController] (action: UIAlertAction!) -> Void in
+//                
+//                self.navigationController?.dismissViewControllerAnimated(true, completion: { () -> Void in
+//                })
+//        }
+//        
+//        alertController.addAction(saveDraftAction)
+//        alertController.addAction(quitAction)
+//        
+//        presentViewController(alertController, animated: true, completion: nil)
+//    }
+    
+    @IBAction func returnToPreviewViewController() {
+        if delegate != nil {
+            delegate?.addRecipients(addedRecipients)
         }
         
-        let quitAction = UIAlertAction(title: "Lukk",
-            style: UIAlertActionStyle.Destructive)
-            { [unowned self, alertController] (action: UIAlertAction!) -> Void in
-                
-                self.navigationController?.dismissViewControllerAnimated(true, completion: { () -> Void in
-                })
-        }
-        
-        alertController.addAction(saveDraftAction)
-        alertController.addAction(quitAction)
-        
-        presentViewController(alertController, animated: true, completion: nil)
-        
+        self.navigationController?.popViewControllerAnimated(true)
     }
 }
