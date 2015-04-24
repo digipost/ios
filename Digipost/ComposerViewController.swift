4b47aa4559925698292ff635bd5e2aca4f6a1c21
Henrik Holmsen <henrik.holmsen@gmail.com>
2015-04-24 08:49:52 +0200

    extract tableview reoder deleagte functions to extension of Composer Viewcontroller

--- Digipost/ComposerViewController.swift
+++ Digipost/ComposerViewController.swift
@@ -42,50 +42,12 @@ class ComposerViewController: UIViewController, ModuleSelectorViewControllerDele
         setupTableView()
     }
     
-    // MARK: - TableView Delegate Reorder functions
-    
-    func tableView(tableView: UITableView!, beganMovingRowAtPoint point: CGPoint, withSnapShotViewOfDraggingRow snapShotView: UIView!) {
-        deleteComposerModuleView = NSBundle.mainBundle().loadNibNamed("DeleteComposerModuleView", owner: self, options: nil)[0] as! DeleteComposerModuleView
-        deleteComposerModuleView.addToView(self.view)
-        deleteComposerModuleView.show()
-        snapShotView.transform = CGAffineTransformMakeRotation(-0.02)
-        let offset = tableView.frame.origin.x
-        snapShotView.frame.size = CGSizeMake(snapShotView.frame.width, 88)
-        snapShotView.frame.origin.x += offset
-        view.addSubview(snapShotView)
-        UIView.animateWithDuration(0.2, animations: { () -> Void in
-            self.tableView.frame.size.height -= self.deleteComposerModuleView.frame.height
-        }) { (Bool) -> Void in
-            self.tableView.setNeedsLayout()
-        }
-        tableView.editing = true
-        
-    }
-    
-    func tableView(tableView: UITableView!, changedPositionOfRowAtPoint point: CGPoint) {
-    }
-    
-    func tableView(tableView: UITableView!, endedMovingRowAtPoint point: CGPoint) {
-        let translatedPoint = tableView.convertPoint(point, toView: deleteComposerModuleView)
-        if deleteComposerModuleView.pointInside(translatedPoint, withEvent: nil){
-            deleteComposerModule()
-        } else {
-            deleteComposerModuleView.hide()
-        }
- 
-        tableView.editing = false
-        
-    }
     
     @IBAction func previewButtonTapped(sender: AnyObject) {
         self.performSegueWithIdentifier("goToPreview", sender: self)
     }
     
-    func deleteComposerModule(){
-        tableView.isDeletingRow = true
-        // Deleting of cell is processed in the UITableView+Reorder Category
-        deleteComposerModuleView.hide()
-    }
+
     
     // MARK: - TableView Setup
     
