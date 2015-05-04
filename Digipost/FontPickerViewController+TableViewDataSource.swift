9310c2f22daa944b9a930ca739c8ff2522e36bfd
Håkon Bogen <hakon.bogen@shortcut.no>
2015-05-04 15:11:30 +0200

    add datasource for font picker

---
+++ Digipost/FontPickerViewController+TableViewDataSource.swift
@@ -0,0 +1,21 @@
+//
+//  FontPickerViewController+TableViewDataSource.swift
+//  Digipost
+//
+//  Created by Håkon Bogen on 04/05/15.
+//  Copyright (c) 2015 Posten. All rights reserved.
+//
+
+import UIKit
+
+extension FontPickerViewController : UITableViewDataSource {
+    
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! UITableViewCell
+        return cell
+    }
+}
