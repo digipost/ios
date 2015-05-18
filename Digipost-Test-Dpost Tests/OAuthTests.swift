e01ff59c8decb5dc5e8a6be2659597def2282360
Håkon Bogen <hakon.bogen@shortcut.no>
2015-05-18 13:56:46 +0200

    make all tests running again

--- Digipost-Test-Dpost Tests/OAuthTests.swift
+++ Digipost-Test-Dpost Tests/OAuthTests.swift
@@ -34,7 +34,7 @@ class OAuthTests: XCTestCase, LUKeychainErrorHandler {
         XCTAssertNil(error, "could not read json")
         return jsonDictionary
     }
-//
+
     func mockTokenWithScope(scope: String) -> OAuthToken {
         var oAuthDictionary: Dictionary<String,AnyObject>!
         if scope == kOauth2ScopeFull {
@@ -45,7 +45,7 @@ class OAuthTests: XCTestCase, LUKeychainErrorHandler {
         let token = OAuthToken(attributes: oAuthDictionary, scope: scope)
         return token!
     }
-//
+
     func testOauthFromDictionary() {
         let oAuthDictionary = jsonDictionaryFromFile("ValidOAuthToken.json")
         let token = OAuthToken(attributes: oAuthDictionary, scope: kOauth2ScopeFull)
@@ -59,92 +59,88 @@ class OAuthTests: XCTestCase, LUKeychainErrorHandler {
         let fierje = OAuthToken.oAuthTokenWithScope(kOauth2ScopeFull)
 
         let token = OAuthToken(refreshToken: "refreifasfkalerjwerw", accessToken: accesstoken, scope: kOauth2ScopeFull, expiresInSeconds: 15)
-        println(token?.refreshToken)
-        println(token)
-        
         let fetchedToken = OAuthToken.oAuthTokenWithScope(kOauth2ScopeFull)
-        println(fetchedToken?.refreshToken)
-        println(fetchedToken?.accessToken)
-        println(fetchedToken?.expires)
-
         XCTAssertNotNil(fetchedToken, "could not store oauthtoken to keychain")
         XCTAssertEqual(fetchedToken!.accessToken!, accesstoken, "wrong accesstoken stored")
+
     }
 
     func keychainAccess(keychainAccess: LUKeychainAccess!, receivedError error: NSError!) {
         println(error)
+        XCTAssertTrue(false, "got an error message \(error) from keychain")
     }
-//    func testMultipleScopedTokensInKeychain() {
-//        let fullToken = mockTokenWithScope(kOauth2ScopeFull)
-//        // create an invalid token that does not get stored
-//        let invalidAuthDictionary = jsonDictionaryFromFile("InvalidOAuthToken.json")
-//        let anotherToken = OAuthToken(attributes: invalidAuthDictionary, scope: "anotherScope")
-//        let allTokens = OAuthToken.oAuthTokens()
-//        XCTAssertTrue(allTokens.count == 1, "token did not correctly store in database")
-//    }
-//    
-//    func testUpdateSameTokenMultipleTimes () {
-//        let newAccessToken = "newAccessToken"
-//        let fullToken = mockTokenWithScope(kOauth2ScopeFull)
-//        
-//        let refetchedToken = OAuthToken.oAuthTokenWithScope(kOauth2ScopeFull)
-//        XCTAssertNotNil(refetchedToken, "no valid token was created")
-//        
-//        refetchedToken!.accessToken = newAccessToken
-//        
-//        let alteredToken = OAuthToken.oAuthTokenWithScope(kOauth2ScopeFull)
-//        let acc = alteredToken?.accessToken!
-//        XCTAssertTrue(alteredToken!.accessToken! == refetchedToken!.accessToken!, "\(alteredToken!.accessToken!) not similar to \(refetchedToken!.accessToken!)")
-//        
-//    }
-//    
-//    func testMultipleScopes() {
-//        let fullToken = mockTokenWithScope(kOauth2ScopeFull)
-//        let fullHighAuth = mockTokenWithScope(kOauth2ScopeFullHighAuth)
-//        let idPorten3 = mockTokenWithScope(kOauth2ScopeFull_Idporten3)
-//        let idPorten4 = mockTokenWithScope(kOauth2ScopeFull_Idporten4)
-//        let allTokens = OAuthToken.oAuthTokens()
-//        XCTAssertNil(idPorten3.refreshToken, "idporten3 token should not have refresh token")
-//        XCTAssertTrue(allTokens.count == 4, "token did not correctly store in database, should be 4, was \(allTokens.count)")
-//    }
-//    
-//    func testdeleteAllTokens () {
-//        let fullToken = mockTokenWithScope(kOauth2ScopeFull)
-//        let fullHighAuth = mockTokenWithScope(kOauth2ScopeFullHighAuth)
-//        let idPorten3 = mockTokenWithScope(kOauth2ScopeFull_Idporten3)
-//        let idPorten4 = mockTokenWithScope(kOauth2ScopeFull_Idporten4)
-//        let allTokens = OAuthToken.oAuthTokens()
-//        XCTAssertTrue(allTokens.count == 4, "token did not correctly store in database, should be 4, was \(allTokens.count)")
-//        
-//        OAuthToken.removeAllTokens()
-//        let allTokensAfterDeletion = OAuthToken.oAuthTokens()
-//        XCTAssertTrue(allTokensAfterDeletion.count == 0, "could not delete token database, should be 0, was \(allTokensAfterDeletion.count)")
-//    }
-//    
-//    func testRenewAccessTokensForMultipleOauthTokens(){
-//        let newAccessTokenFull = "new Acesstoken for Full"
-//        let newAccessTokenHighAuth = "new Acesstoken for HighAuth"
-//        let newAccessTokenIdPorten4 = "new Acesstoken for idporten4"
-//        let fullToken = mockTokenWithScope(kOauth2ScopeFull)
-//        let fullHighAuth = mockTokenWithScope(kOauth2ScopeFullHighAuth)
-//        let idPorten3 = mockTokenWithScope(kOauth2ScopeFull_Idporten3)
-//        let idPorten4 = mockTokenWithScope(kOauth2ScopeFull_Idporten4)
-//        
-//        
-//        let fetchedFull = OAuthToken.oAuthTokenWithScope(kOauth2ScopeFull)
-//        let fetchedHighAuth = OAuthToken.oAuthTokenWithScope(kOauth2ScopeFullHighAuth)
-//        let fetchedIdPorten4 = OAuthToken.oAuthTokenWithScope(kOauth2ScopeFull_Idporten4)
-//        
-//        fetchedFull?.accessToken = newAccessTokenFull
-//        fetchedHighAuth?.accessToken = newAccessTokenHighAuth
-//        fetchedIdPorten4?.accessToken = newAccessTokenIdPorten4
-//        
-//        let refetchedFull = OAuthToken.oAuthTokenWithScope(kOauth2ScopeFull)
-//        XCTAssertEqual(refetchedFull!.accessToken!, newAccessTokenFull, "did not save new access token correctly")
-//        let refetchedHighAuth = OAuthToken.oAuthTokenWithScope(kOauth2ScopeFullHighAuth)
-//        XCTAssertEqual(refetchedHighAuth!.accessToken!, newAccessTokenHighAuth, "did not save new access token correctly")
-//        
-//        let refetchedIdporten4 = OAuthToken.oAuthTokenWithScope(kOauth2ScopeFull_Idporten4)
-//        XCTAssertEqual(refetchedIdporten4!.accessToken!, newAccessTokenIdPorten4, "did not save new access token correctly")
-//    }
+
+    func testMultipleScopedTokensInKeychain() {
+        let fullToken = mockTokenWithScope(kOauth2ScopeFull)
+        // create an invalid token that does not get stored
+        let invalidAuthDictionary = jsonDictionaryFromFile("ValidOAuthToken.json")
+        let anotherToken = OAuthToken(attributes: invalidAuthDictionary, scope: kOauth2ScopeFullHighAuth)
+        let allTokens = OAuthToken.oAuthTokens()
+        XCTAssertTrue(allTokens.count == 2, "token did not correctly store in database")
+    }
+
+    // creates a token, fetches it from keychain, adds a new access token, then refetches to see if it did get updated
+    func testUpdateSameTokenMultipleTimes () {
+        let newAccessToken = "newAccessToken"
+        let fullToken = mockTokenWithScope(kOauth2ScopeFull)
+        
+        let refetchedToken = OAuthToken.oAuthTokenWithScope(kOauth2ScopeFull)
+        XCTAssertNotNil(refetchedToken, "no valid token was created")
+        
+        refetchedToken!.accessToken = newAccessToken
+        
+        let alteredToken = OAuthToken.oAuthTokenWithScope(kOauth2ScopeFull)
+        let acc = alteredToken?.accessToken!
+        XCTAssertTrue(alteredToken!.accessToken! == refetchedToken!.accessToken!, "\(alteredToken!.accessToken!) not similar to \(refetchedToken!.accessToken!)")
+    }
+
+    func testMultipleScopes() {
+        let fullToken = mockTokenWithScope(kOauth2ScopeFull)
+        let fullHighAuth = mockTokenWithScope(kOauth2ScopeFullHighAuth)
+        let idPorten3 = mockTokenWithScope(kOauth2ScopeFull_Idporten3)
+        let idPorten4 = mockTokenWithScope(kOauth2ScopeFull_Idporten4)
+        let allTokens = OAuthToken.oAuthTokens()
+        XCTAssertNil(idPorten3.refreshToken, "idporten3 token should not have refresh token")
+        XCTAssertTrue(allTokens.count == 4, "token did not correctly store in database, should be 4, was \(allTokens.count)")
+    }
+
+    func testdeleteAllTokens () {
+        let fullToken = mockTokenWithScope(kOauth2ScopeFull)
+        let fullHighAuth = mockTokenWithScope(kOauth2ScopeFullHighAuth)
+        let idPorten3 = mockTokenWithScope(kOauth2ScopeFull_Idporten3)
+        let idPorten4 = mockTokenWithScope(kOauth2ScopeFull_Idporten4)
+        let allTokens = OAuthToken.oAuthTokens()
+        XCTAssertTrue(allTokens.count == 4, "token did not correctly store in database, should be 4, was \(allTokens.count)")
+        
+        OAuthToken.removeAllTokens()
+        let allTokensAfterDeletion = OAuthToken.oAuthTokens()
+        XCTAssertTrue(allTokensAfterDeletion.count == 0, "could not delete token database, should be 0, was \(allTokensAfterDeletion.count)")
+    }
+
+    func testRenewAccessTokensForMultipleOauthTokens(){
+        let newAccessTokenFull = "new Acesstoken for Full"
+        let newAccessTokenHighAuth = "new Acesstoken for HighAuth"
+        let newAccessTokenIdPorten4 = "new Acesstoken for idporten4"
+        let fullToken = mockTokenWithScope(kOauth2ScopeFull)
+        let fullHighAuth = mockTokenWithScope(kOauth2ScopeFullHighAuth)
+        let idPorten3 = mockTokenWithScope(kOauth2ScopeFull_Idporten3)
+        let idPorten4 = mockTokenWithScope(kOauth2ScopeFull_Idporten4)
+        
+        
+        let fetchedFull = OAuthToken.oAuthTokenWithScope(kOauth2ScopeFull)
+        let fetchedHighAuth = OAuthToken.oAuthTokenWithScope(kOauth2ScopeFullHighAuth)
+        let fetchedIdPorten4 = OAuthToken.oAuthTokenWithScope(kOauth2ScopeFull_Idporten4)
+        
+        fetchedFull?.accessToken = newAccessTokenFull
+        fetchedHighAuth?.accessToken = newAccessTokenHighAuth
+        fetchedIdPorten4?.accessToken = newAccessTokenIdPorten4
+        
+        let refetchedFull = OAuthToken.oAuthTokenWithScope(kOauth2ScopeFull)
+        XCTAssertEqual(refetchedFull!.accessToken!, newAccessTokenFull, "did not save new access token correctly")
+        let refetchedHighAuth = OAuthToken.oAuthTokenWithScope(kOauth2ScopeFullHighAuth)
+        XCTAssertEqual(refetchedHighAuth!.accessToken!, newAccessTokenHighAuth, "did not save new access token correctly")
+        
+        let refetchedIdporten4 = OAuthToken.oAuthTokenWithScope(kOauth2ScopeFull_Idporten4)
+        XCTAssertEqual(refetchedIdporten4!.accessToken!, newAccessTokenIdPorten4, "did not save new access token correctly")
+    }
 }
