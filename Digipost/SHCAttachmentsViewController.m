f055d778f7cf4f359d627cb2717d9d37470c4d91
Håkon Bogen <hakon.bogen@shortcut.no>
2015-06-09 12:47:21 +0200

    fix crash

--- Digipost/SHCAttachmentsViewController.m
+++ Digipost/SHCAttachmentsViewController.m
@@ -229,7 +229,11 @@ NSString *const kAttachmentsViewControllerScreenName = @"Attachments";
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    if (self.attachments.count == 0 ) {
+        self.attachments = [self attachmentsForCurrentDocument];
+    }
     POSAttachment *attachment = self.attachments[indexPath.row];
+
 
     //    if (attachment.openingReceiptUri) {
     //        [UIAlertView showWithTitle:NSLocalizedString(@"Avsender krever lesekvittering", @"Avsender krever lesekvittering")
