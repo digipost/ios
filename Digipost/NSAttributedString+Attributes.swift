80066ae5d7c1488e35be9bd170bc224271f62dc0
Håkon Bogen <hakon.bogen@shortcut.no>
2015-07-13 15:14:15 +0200

    add convenience methods for checking styles of an attributed string

---
+++ Digipost/NSAttributedString+Attributes.swift
@@ -0,0 +1,43 @@
+//
+//  NSAttributedString+Attributes.swift
+//  Digipost
+//
+//  Created by Håkon Bogen on 13/07/15.
+//  Copyright (c) 2015 Posten. All rights reserved.
+//
+
+import UIKit
+
+extension NSAttributedString {
+
+
+
+    func symbolicTraits() -> UIFontDescriptorSymbolicTraits {
+        var symbolicTraits = UIFontDescriptorSymbolicTraits.allZeros
+
+        self.enumerateAttribute(NSFontAttributeName, inRange: NSMakeRange(0, self.length), options: NSAttributedStringEnumerationOptions.LongestEffectiveRangeNotRequired) { (attribute, range, stop) -> Void in
+            if let font = attribute as? UIFont {
+                symbolicTraits = font.fontDescriptor().symbolicTraits
+                stop.memory = true
+            }
+        }
+        return symbolicTraits
+    }
+
+    func isBold() -> Bool {
+        let symbolicTraits = self.symbolicTraits()
+        if symbolicTraits & .TraitBold == .TraitBold {
+            return true
+        }
+        return false
+    }
+
+    func isItalic() -> Bool {
+        let symbolicTraits = self.symbolicTraits()
+        if symbolicTraits & .TraitItalic == .TraitItalic {
+            return true
+        }
+        return false
+    }
+ 
+}
