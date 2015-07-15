4ed5656c35d8b92d828526961c4cc29f99dae0e5
Håkon Bogen <hakon.bogen@shortcut.no>
2015-07-15 17:26:35 +0200

    add model for a section of html content

---
+++ Digipost/HTMLSection.swift
@@ -0,0 +1,13 @@
+//
+//  HTMLSection.swift
+//  Digipost
+//
+//  Created by Håkon Bogen on 15/07/15.
+//  Copyright (c) 2015 Posten. All rights reserved.
+//
+
+import UIKit
+
+class HTMLSection: NSObject {
+   
+}
