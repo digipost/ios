f46a50d89d2eb59a0b542ab28ebf082fa9061526
Fredrik Lillejordet <fretholil@gmail.com>
2016-03-14 10:30:27 +0100

    Setter riktig lisens-header for alle filer
    Det har vært brukt feil oppsett ved utvikling av appen, som gjør at lisensinformasjonen i mange filer har vært feil. Riktig lisens er, og har alltid vært, Apache 2.0.

--- Digipost/POSNewFolderViewController.m
+++ Digipost/POSNewFolderViewController.m
@@ -1,9 +1,17 @@
 //
-//  POSNewFolderViewController.m
-//  Digipost
+// Copyright (C) Posten Norge AS
 //
-//  Created by Håkon Bogen on 26.05.14.
-//  Copyright (c) 2014 Posten. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//         http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
 
 #import "UIColor+Convenience.h"
