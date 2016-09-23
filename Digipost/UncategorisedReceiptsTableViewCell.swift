828dfdd16dce2e385b9bda47217c60ed2ebe2910
William Berg <william.berg@bekk.no>
2016-09-23 16:03:34 +0200

    Remove connections to old POSReceiptTableViewCell

---
+++ Digipost/UncategorisedReceiptsTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  UncategorisedReceiptsTableViewCell.swift
+//  Digipost
+//
+//  Created by William Berg on 23/09/16.
+//  Copyright © 2016 Posten Norge AS. All rights reserved.
+//
+
+import Foundation
