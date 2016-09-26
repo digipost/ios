f9e91777d516e68d1b96105e6cb4e6962f9a3516
William Berg <william.berg@bekk.no>
2016-09-26 16:27:45 +0200

    Add Uncategorised receipts VC as former VC

--- Digipost/POSLetterViewController.h
+++ Digipost/POSLetterViewController.h
@@ -26,11 +26,13 @@ extern NSString *const kPushReceiptIdentifier;
 @class POSReceiptFoldersTableViewController;
 @class POSAttachment;
 @class POSReceipt;
+@class UncategorisedReceiptsViewController;
 
 @interface POSLetterViewController : GAITrackedViewController <UISplitViewControllerDelegate, UIGestureRecognizerDelegate>
 
 @property (weak, nonatomic) POSDocumentsViewController *documentsViewController;
-@property (weak, nonatomic) POSReceiptFoldersTableViewController *receiptsViewController;
+//@property (weak, nonatomic) POSReceiptFoldersTableViewController *receiptsViewController;
+@property UncategorisedReceiptsViewController *receiptsViewController;
 @property (strong, nonatomic) POSAttachment *attachment;
 @property (strong, nonatomic) POSReceipt *receipt;
 @property (strong, nonatomic) UIPopoverController *masterViewControllerPopoverController;
