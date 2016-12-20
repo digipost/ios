e713869f3a976a8b387cb5689eedd409735a86b0
Fredrik Lillejordet <fretholil@gmail.com>
2016-12-20 14:34:19 +0100

    endret bakgrunnsfarge i mappelista

--- Digipost/POSFoldersViewController.m
+++ Digipost/POSFoldersViewController.m
@@ -288,10 +288,7 @@ NSString *const kEditFolderSegue = @"newFolderSegue";
         }
     }
 
-    cell.backgroundColor = [UIColor colorWithRed:64.0 / 255.0
-                                           green:66.0 / 255.0
-                                            blue:69.0 / 255.0
-                                           alpha:1.0];
+    cell.backgroundColor = [UIColor colorWithRed:0.24 green:0.24 blue:0.25 alpha:1.0];
     cell.folderNameLabel.text = folderName;
     cell.iconImageView.image = iconImage;
     cell.arrowImageView.hidden = arrowHidden;
@@ -476,9 +473,7 @@ NSString *const kEditFolderSegue = @"newFolderSegue";
                                                             CGRectGetWidth(tableView.frame),
                                                             headerHeight)];
     [view addSubview:headerLabel];
-    [view setBackgroundColor:[UIColor pos_colorWithR:64
-                                                   G:66
-                                                   B:69]];
+    [view setBackgroundColor:[UIColor colorWithRed:0.24 green:0.24 blue:0.25 alpha:1.0]];
 
     return view;
 }
