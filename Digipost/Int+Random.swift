5f042ffb7c48b87e0638ab5516dc24ed003a1e8f
Fredrik Lillejordet <fretholil@gmail.com>
2017-01-02 09:48:39 +0100

    Byttet Range med CountableClosedRange i Int.random implementasjon etter fjerning av Range i swift(3.0)

--- Digipost/Int+Random.swift
+++ Digipost/Int+Random.swift
@@ -18,17 +18,7 @@ import UIKit
 
 extension Int {
     static func random(_ range: Range<Int> ) -> Int {
-        var offset = 0
-
-        if range.lowerBound < 0
-        {
-            offset = abs(range.lowerBound)
-        }
-
-        let mini = UInt32(range.lowerBound + offset)
-        let maxi = UInt32(range.upperBound   + offset)
-
-        return Int(mini + arc4random_uniform(maxi - mini)) - offset
+        let count = UInt32(range.upperBound - range.lowerBound)
+        return Int(arc4random_uniform(count)) + range.lowerBound
     }
-
 }
