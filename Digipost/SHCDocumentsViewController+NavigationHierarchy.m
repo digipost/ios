cc92ef793aa24aef2e6a5bc94b5729b56a7d838f
Fredrik Lillejordet <fretholil@gmail.com>
2017-01-23 13:12:08 +0100

    Skiller ikke VC-Hierarki mellom iPad og iPad

--- Digipost/SHCDocumentsViewController+NavigationHierarchy.m
+++ Digipost/SHCDocumentsViewController+NavigationHierarchy.m
@@ -21,49 +21,25 @@
 @implementation POSDocumentsViewController (NavigationHierarchy)
 - (void)addAccountsAnFoldersVCToDoucmentHierarchy
 {
-
-    if ([UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPhone) {
-        if ([self.navigationController.viewControllers[1] isKindOfClass:[AccountViewController class]] == NO) {
-            AccountViewController *accountViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"accountViewController"];
-            POSFoldersViewController *folderViewController = [self.storyboard instantiateViewControllerWithIdentifier:kFoldersViewControllerIdentifier];
-
-            NSMutableArray *newViewControllerArray = [NSMutableArray array];
-            // add account vc as second view controller in navigation controller
-            UIViewController *loginViewController = self.navigationController.viewControllers[0];
-
-            POSDocumentsViewController *currentViewController = self.navigationController.viewControllers.lastObject;
-
-            [newViewControllerArray addObject:loginViewController];
-            [newViewControllerArray addObject:accountViewController];
-            [newViewControllerArray addObject:folderViewController];
-            [newViewControllerArray addObject:currentViewController];
-
-            folderViewController.selectedMailBoxDigipostAdress = currentViewController.mailboxDigipostAddress;
-
-            [self.navigationController setViewControllers:newViewControllerArray
-                                                 animated:YES];
-        }
-    } else {
-        if ([self.navigationController.viewControllers[1] isKindOfClass:[POSFoldersViewController class]] == NO) {
-            POSFoldersViewController *folderViewController = [self.storyboard instantiateViewControllerWithIdentifier:kFoldersViewControllerIdentifier];
-            NSMutableArray *newViewControllerArray = [NSMutableArray array];
-            // add account vc as second view controller in navigation controller
-            AccountViewController *accountVC = self.navigationController.viewControllers[0];
-            if ([accountVC isKindOfClass:[AccountViewController class]] == NO) {
-                accountVC = [self.storyboard instantiateViewControllerWithIdentifier:@"accountViewController"];
-            }
-
-            POSDocumentsViewController *currentViewController = self.navigationController.viewControllers.lastObject;
-
-            [newViewControllerArray addObject:accountVC];
-            [newViewControllerArray addObject:folderViewController];
-            [newViewControllerArray addObject:currentViewController];
-
-            folderViewController.selectedMailBoxDigipostAdress = currentViewController.mailboxDigipostAddress;
-
-            [self.navigationController setViewControllers:newViewControllerArray
-                                                 animated:NO];
-        }
+    if ([self.navigationController.viewControllers[1] isKindOfClass:[AccountViewController class]] == NO) {
+        AccountViewController *accountViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"accountViewController"];
+        POSFoldersViewController *folderViewController = [self.storyboard instantiateViewControllerWithIdentifier:kFoldersViewControllerIdentifier];
+        
+        NSMutableArray *newViewControllerArray = [NSMutableArray array];
+        // add account vc as second view controller in navigation controller
+        UIViewController *loginViewController = self.navigationController.viewControllers[0];
+        
+        POSDocumentsViewController *currentViewController = self.navigationController.viewControllers.lastObject;
+        
+        [newViewControllerArray addObject:loginViewController];
+        [newViewControllerArray addObject:accountViewController];
+        [newViewControllerArray addObject:folderViewController];
+        [newViewControllerArray addObject:currentViewController];
+        
+        folderViewController.selectedMailBoxDigipostAdress = currentViewController.mailboxDigipostAddress;
+        
+        [self.navigationController setViewControllers:newViewControllerArray
+                                             animated:YES];
     }
 }
 @end
