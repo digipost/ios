776ea8e470822668f8f51c6e23f71f9e391a45a4
Fredrik Lillejordet <fretholil@gmail.com>
2017-07-05 15:57:59 +0200

    lagt til view+viewcontroller for innkalling

---
+++ Digipost/AppointmentView.swift
@@ -0,0 +1,46 @@
+//
+// Copyright (C) Posten Norge AS
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//         http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+
+import UIKit
+
+@objc class AppointmentView: UIView {
+    
+    @IBOutlet weak var subjectLabel: UILabel!
+    
+    func instanceWithData(appointment: POSAppointment) -> UIView{
+        let view = UINib(nibName: "AppointmentView", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as! AppointmentView 
+        print(appointment.title)
+        view.subjectLabel.text = appointment.title
+        return view
+    }
+    
+    private  func instanceFromNib() -> UIView {
+        return UINib(nibName: "AppointmentView", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as! UIView
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override public var intrinsicContentSize: CGSize {
+        return CGSize(width: 300, height: 5)
+    }
+}
